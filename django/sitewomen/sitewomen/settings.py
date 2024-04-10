3245f6a0a4f6c1b02217f90e88e511beb8245b80
JackCodder <not.a.jack.shenzhen@gmail.com>
2024-04-10 20:48:56 +0500

    1. GET и POST запросы 2. Обработчик исключения 404

--- django/sitewomen/sitewomen/settings.py
+++ django/sitewomen/sitewomen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--+$wn!0g3!)330w*wymj7jx3(1zdgq93yk_r@(6mi9kjhb0x2u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
