9afbd5208d3b1ed79b168b6e54a87901daa47ca5
JackCodder <not.a.jack.shenzhen@gmail.com>
2024-04-10 08:35:34 +0500

    1. Добавление проекта в настройки Run/Debug Configuration 2. Динамические URL для маршрута 'cats/(int/slug)' 3. Создание собственного конвертера

--- django/sitewomen/women/urls.py
+++ django/sitewomen/women/urls.py
@@ -1,7 +1,12 @@
-from django.urls import include, path
+from django.urls import include, path, re_path, register_converter
 from . import views
+from . import converters
+
+register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.index), #http://127.0.0.1:8000/
-    path('cats/', views.categories), #http://127.0.0.1:8000/cats/
+    path('cats/<int:cat_id>/', views.categories), #http://127.0.0.1:8000/cats/2
+    path('cats/<slug:cat_slug>/', views.categories_by_slug), #http://127.0.0.1:8000/cats/ad2
+    path('archive/<year4:year>/', views.archive), #http://127.0.0.1:8000/archive/2024
 ]
--- django/sitewomen/women/views.py
+++ django/sitewomen/women/views.py
@@ -5,5 +5,13 @@ def index(request): #HttpRequest
     return HttpResponse('Страница приложения women')
 
 
-def categories(reques):
-    return HttpResponse('<h1>Статьи по категориям</h1>')
+def categories(reques, cat_id):
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>id:{cat_id}</p>')
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug:{cat_slug}</p>')
+
+
+def archive(request, year):
+    return HttpResponse((f"<h1>Архив по годам</h1><p >{year}</p>"))
