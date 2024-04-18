bfe3854783df467c339c94027b932ef382478f17
JackCodder <not.a.jack.shenzhen@gmail.com>
2024-04-18 22:58:22 +0500

    TEST COMMIT

--- django/sitewomen/women/views.py
+++ django/sitewomen/women/views.py
@@ -17,7 +17,7 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2024:
-        return redirect('cats', 'music')
+        return redirect('cats', 'moves')
     return HttpResponse((f"<h1>Архив по годам</h1><p >{year}</p>"))
 
 def page_not_found(request, exception):
