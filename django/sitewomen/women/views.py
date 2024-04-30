de3c36222b00e8f6155bf0d5da48cf3be2638dfc
JackCodder <not.a.jack.shenzhen@gmail.com>
2024-04-30 11:38:31 +0500

    Передача данных (переменных) в шаблоны

--- django/sitewomen/women/views.py
+++ django/sitewomen/women/views.py
@@ -3,14 +3,31 @@ from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
 
 
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
 def index(request): #HttpRequest
     # t = render_to_string('women/index.html')
-    # return HttpResponse(t) # текстовый вариант шаблона 
-    return render(request, 'women/index.html')
+    # return HttpResponse(t) # текстовый вариант шаблона
+    data = {'title': 'Главная страница',
+            'menu': menu,
+            'float': 28.56,
+            'lst': [1, 2, 'abc', True],
+            'set': {1, 2, 3, 2, 5},
+            'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+            'obj': MyClass(10, 20),
+            }
+    return render(request, 'women/index.html', context=data)
 
 
 def about(request):
-    return render(request, 'women/about.html')
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
 def categories(reques, cat_id):
